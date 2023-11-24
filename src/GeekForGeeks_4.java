0dd1f4b19d565150b55f6be5f1faed77e9994051
MudassirQuraishi <mudassir.quraishi14@outlook.com>
2023-11-24 18:33:49 +0530

    Missing number in arithmetic progression

--- src/GeekForGeeks_4.java
+++ src/GeekForGeeks_4.java
@@ -2,20 +2,15 @@ public class GeekForGeeks_4 {
     public static int findMissing(int[] arr, int n) {
         // code here
         int commonDiff = (arr[arr.length - 1] - arr[0]) / arr.length;
-
         int left = 0, right = arr.length - 1;
-
         while (left < right) {
             int mid = left + (right - left) / 2;
-
             if (arr[mid] + commonDiff != arr[mid + 1]) {
                 return arr[mid] + commonDiff;
             }
-
             if (arr[mid] - commonDiff != arr[mid - 1]) {
                 return arr[mid] - commonDiff;
             }
-
             if (arr[mid] == arr[0] + mid * commonDiff) {
                 left = mid + 1;
             } else {
